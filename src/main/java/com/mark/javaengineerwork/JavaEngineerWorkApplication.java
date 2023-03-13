3a831202207988eef91dddf0076dcc36ea6ac5f9
EMP2112475\markchiang <s22027761S>
2023-03-13 17:40:29 +0800

    git init

---
+++ src/main/java/com/mark/javaengineerwork/JavaEngineerWorkApplication.java
@@ -0,0 +1,13 @@
+package com.mark.javaengineerwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEngineerWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEngineerWorkApplication.class, args);
+    }
+
+}
