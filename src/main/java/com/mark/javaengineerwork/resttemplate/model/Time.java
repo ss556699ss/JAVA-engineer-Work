4aca963e5e2f7bc463ec4fd190e989e1fc259a02
EMP2112475\markchiang <s22027761S>
2023-03-14 22:15:27 +0800

    2023/03/14 呼叫 coinDesk 的 API /更新時間/取得幣別相關資訊

---
+++ src/main/java/com/mark/javaengineerwork/resttemplate/model/Time.java
@@ -0,0 +1,19 @@
+package com.mark.javaengineerwork.resttemplate.model;
+
+import java.util.Date;
+
+public class Time {
+
+    public String updatedISO;
+
+
+    public String getUpdatedISO() {
+        return updatedISO;
+    }
+
+    public void setUpdatedISO(String updatedISO) {
+        this.updatedISO = updatedISO;
+    }
+
+
+}
